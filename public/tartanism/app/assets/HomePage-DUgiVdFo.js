e83e362dfd07e4a8b8f77ed5787accb947adb531
David Murray <54763683+thedavidmurray@users.noreply.github.com>
2026-05-11 18:20:13 -0700

    SPA routing fix: 404.html redirect handler + React Router _p param support

---
+++ public/tartanism/app/assets/HomePage-DUgiVdFo.js
@@ -0,0 +1,2 @@
+import{j as r}from"./index-3ZW5epyv.js";import o from"./LibraryPage-B2So_0hK.js";import"./tartanLibrary-Bp6usyiL.js";import"./weaves-09Ea8xod.js";import"./renderTartan-BFHpeAS5.js";function a(){return r.jsx(o,{})}export{a as default};
+//# sourceMappingURL=HomePage-DUgiVdFo.js.map
